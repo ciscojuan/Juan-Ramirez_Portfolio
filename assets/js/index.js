f261e2274d8b434f167b10adda22e5ea335b53fb
juan Ramirez <ciscojuan@gmail.com>
2023-03-08 18:07:15 -0500

    adding a new feature: blur when show menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,47 @@
 "use strict";
 const darkSwitch = document.querySelector(".darkSwitch");
 const theme = document.querySelector("#darkMode");
-const menu = document.querySelector('.aside__perfil');
-const openMenu = document.querySelector('[data-greather]');
-const closeMenu = document.querySelector('[data-lower]');
-console.log(openMenu)
-console.log(closeMenu)
+const container = document.querySelector('.container__sections')
+/* ################################## menu ############################ */
+const buttonMenu = document.querySelector(".button__container");
+const menu = document.querySelector(".aside__perfil");
+const arrowOpen = document.querySelector("[data-greather]");
+const arrowClose = document.querySelector("[data-lower]");
+let isOpeneded = false;
 
 
- openMenu.addEventListener('click', () => {
-    menu.style.display = 'block';
-    closeMenu.style.display = 'block';
-    openMenu.style.display = 'none';
+buttonMenu.addEventListener("click", (e) => {
 
-    setTimeout(function() {
-        menu.style.opacity = "1";
-        menu.style.transform = "translateX(0)";
-      }, 10);
-      menu.style.transition = "all 2s ease";
-}) 
+  isOpeneded =!isOpeneded;
+  if(isOpeneded == true && document.documentElement.clientWidth <= 768){
+      menu.style.display = "block";
+      arrowClose.style.display = "block";
+      arrowOpen.style.display = "none";
+      container.style.filter = "blur(2.5px)";
+  }else{
+    menu.style.display = "none";
+    arrowClose.style.display = "none";
+    arrowOpen.style.display = "block";
+    container.style.filter = "blur(0)";
+  } e.stopImmediatePropagation()
+});
 
-closeMenu.addEventListener('click', () => {
-    menu.style.display = 'none';
-    openMenu.style.display = 'block';
-    closeMenu.style.display = 'none';
-
-    setTimeout(function() {
-        menu.style.opacity = "0";
-        menu.style.transform = "translateX(-100%)";
-      }, 10);
-    menu.style.transition = "all 2s ease";
-
-})
 // Load theme on page load
-window.addEventListener('load', () => {
-    var themeMode = localStorage.getItem('theme');
+window.addEventListener("load", () => {
+  var themeMode = localStorage.getItem("theme");
 
-    console.log(themeMode)
-    if (themeMode === 'dark') {
-        theme.href = 'assets/css/darkmode/darkmode.css';
-        darkSwitch.checked = true;
-    } else {
-        theme.href = 'assets/css/lightMode/lightmode.css';
-    }
+  console.log(themeMode);
+  if (themeMode === "dark") {
+    theme.href = "assets/css/darkmode/darkmode.css";
+    darkSwitch.checked = true;
+  } else {
+    theme.href = "assets/css/lightMode/lightmode.css";
+  }
 });
 
 // Toggle theme on darkSwitch change
-darkSwitch.addEventListener('change', () => {
-    const themeMode = darkSwitch.checked ? 'dark' : 'light';
-    theme.href = `assets/css/${themeMode}mode/${themeMode}mode.css`;
-    localStorage.setItem('theme', themeMode);
+darkSwitch.addEventListener("change", () => {
+  const themeMode = darkSwitch.checked ? "dark" : "light";
+  theme.href = `assets/css/${themeMode}mode/${themeMode}mode.css`;
+  localStorage.setItem("theme", themeMode);
 });
